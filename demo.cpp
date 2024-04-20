3ff67bfeafd95cff1b087ef883551de5a27e0ac0
anand-marikanti <anand.cpp15@gmail.com>
2024-04-20 21:27:08 +0530

    demo code of cpp

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define PI 3.14
+
+int main()
+{
+    int a=10;
+    std::cout<<"Hello Guys"<<std::endl;
+    std::cout<<"size of a is "<<sizeof(a)<<std::endl;
+    std::cout<<"value of PI is "<<PI<<std::endl;
+    return 0;
+}
