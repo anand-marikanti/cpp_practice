f9b67c24b4352bfa7b9495ae97cb47ef47dc86d5
anand-marikanti <anand.cpp15@gmail.com>
2024-04-21 17:40:09 +0530

    programe for the sum of two numbers

---
+++ sum_of_num.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout << "Enter the value first value " << endl;
+    cin >> a;
+    cout << "Enter the value Second value " << endl;
+    cin >> b;
+    c = a + b;
+    cout << "Sum of the a and b ="
+         << " " << c << endl;
+}
